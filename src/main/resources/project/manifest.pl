77e5044aff0ee96e199900034d6e55a9ee31ef35
Valentyn Solomko <Valentyn.Solomko@portaone.com>
2019-01-08 11:54:23 +0200

    Adapt for building with Gradle

--- src/main/resources/project/manifest.pl
+++
@@ -1,6 +0,0 @@
-@files = (
-    ['//property[propertyName="jtestDriver"]/value', 'jtestDriver.pl'],
-    ['//property[propertyName="postp_matchers"]/value', 'postp_matchers.pl'],
-	['//procedure[procedureName="runJtest"]/propertySheet/property[propertyName="ec_parameterForm"]/value', 'wizardCustomEditor.xml'],
-    ['//property[propertyName="ec_setup"]/value', 'ec_setup.pl'],
-);
